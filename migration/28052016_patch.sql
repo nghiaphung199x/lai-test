caf77fa1731abbafd20d0f7ba4b017f77d7eee45
cuonghv <cuonghv.1506@gmail.com>
2016-05-28 01:15:32 +0700

    UPdate sql patch

--- migration/28052016_patch.sql
+++ migration/28052016_patch.sql
@@ -1,4 +1,6 @@
 
+ALTER TABLE `phppos_receivings_items` ADD COLUMN `measure_id`  int(11) NULL AFTER `quantity_received`;
+
 ALTER TABLE `phppos_receivings_items`
 ADD COLUMN `measure_qty`  int(11) NULL AFTER `measure_id`;
 ALTER TABLE `phppos_receivings_items`
