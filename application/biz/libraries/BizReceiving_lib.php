047a635eca9e5948642556530e62c765bef9ea34
cuonghv <cuonghv.1506@gmail.com>
2016-07-05 03:14:13 +0700

    fix bugs.

--- application/biz/libraries/BizReceiving_lib.php
+++ application/biz/libraries/BizReceiving_lib.php
@@ -117,7 +117,7 @@ class BizReceiving_lib extends Receiving_lib
 		}
 		
 		$measure = $this->CI->Measure->getInfo($cur_item_info->measure_id);
-		
+		$quantity = $quantity_received;
 		if ($receiving_id) {
 			$measureOnRecv = $this->CI->Receiving->getMeasureOnRecvItem($receiving_id, $item_id);
 			if ($measureOnRecv) {
