557d772229542b2d8e158b73b9cff727e15b49e2
nghiaphung199x <nghiaphung1990@gmail.com>
2016-10-26 18:51:30 +0700

    123

--- application/biz/views/tasks/_addform_view.php
+++ application/biz/views/tasks/_addform_view.php
@@ -135,6 +135,7 @@
 							</div>
 						</div>
 					</div>
+                    <!-- nghiavl@115.146.126.125:/opt/repos/4biz2016.git -->
 					<div class="clearfix hang">
 						<div id="add_navigation">
 							<div class="title active" style="border-top: 1px solid #ccc;" data-id="thietlap_content">Thông tin</div>
